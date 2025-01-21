62b3b3e4e4f160504ca8ebe22be5194555500d43
shifna majeed <sshifnamajeed@gmail.com>
2025-01-21 22:58:42 +0530

    updated

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,7 +4,7 @@ import express from 'express'
 import cors from 'cors'
 import {connectDB} from './Config/db.js';
 import morgan from 'morgan'
-import userRouter from './Router/ProductRoutes.js'
+import ProductRouter from './Router/ProductRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -21,7 +21,7 @@ app.use(cors({
 
 app.use(morgan('dev'))
 
-app.use('/', userRouter)
+app.use('/', ProductRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
